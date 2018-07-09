b45b963019d001596559cf23f93f00db02383ae7
voorevamshi <37767342+voorevamshi@users.noreply.github.com>
2018-07-09 07:50:23 +0530

    Added userId for Setter and Getter

--- User.java
+++ User.java
@@ -1,4 +1,11 @@
 public class User{
 private int userId;
 private String userName;
+  public void setUserId(int UserId){
+    this.userId=userId
+  }
+  public int getUserId(){
+  retrun userId
+  }
 }
+
