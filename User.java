0ef2452438c81b938b39bbf888d2080b65a6a783
voorevamshi <37767342+voorevamshi@users.noreply.github.com>
2023-02-09 10:12:23 +0530

    Fixed CE

--- User.java
+++ User.java
@@ -3,23 +3,17 @@ private int userId;
 private String userName;
 private byte age;
 
-  public void setUserId(int UserId){
-    this.userId=userId
-  }
-  public int getUserId(){
-  retrun userId
-  }
+
  public void setUserName(String userName){
- this.userName=userName
+ this.userName=userName;
  }
  public String getUserName(){
- return userName}
+ return userName;}
 
-  //comments
+  
 public static void main(String[] arg){
   User u1=new User();
   u1.setUserName("Vamshi");
 System.out.println("My name is: "+u1.getUserName());
 }
 }
-
